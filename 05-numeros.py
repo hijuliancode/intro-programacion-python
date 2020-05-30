f30bb95d4299068d8b191d2239752640a169d731
JulianCode <hijuliansosa@protonmail.com>
2020-05-30 15:41:07 -0500

    Números y ejemplos con números

---
+++ 05-numeros.py
@@ -0,0 +1,23 @@
+print(2)
+print(2.5)
+
+print(' ')
+#operaciones
+
+print(3 + 2) # operando(3) operador(+) operando(2)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+
+print(4 ** 4) # ((4 * 4) * 4) * 4
+print(3 ** 3) # ((3 * 3) * 3)
+
+# Python también soporta el orden de las operaciones
+print( (2 + 3) * 4 ) # 20 porque ((2 + 3) = 5) * 4 = 20
+
+# Pordemos incrementar números con += no con ++
+numero = 20
+print(numero)
+# numero++ # Da error
+numero += 2
+print(numero)
