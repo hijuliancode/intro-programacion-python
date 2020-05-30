c0c42e052dd4ad7903290f8e22bc6c8d6ac169ca
JulianCode <hijuliansosa@protonmail.com>
2020-05-30 15:48:48 -0500

    Funciones con Números

---
+++ 05-numeros-y-funciones.py
@@ -0,0 +1,17 @@
+## Funciones
+def suma(a = 0, b = 0):
+  print(f'Suma', a + b)
+
+def resta(a = 0, b = 0):
+  print(f'Resta', a - b)
+
+def multiplicacion(a = 0, b = 0):
+  print(f'Multiplicacion', a * b)
+
+def division(a = 0, b = 0):
+  print(f'Division', a / b)
+
+suma(2, 4)
+resta(4, 4)
+multiplicacion(5, 4)
+division(8, 4)
