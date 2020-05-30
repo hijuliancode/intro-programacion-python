f3008341ddfb373315d20732060c52b139f52877
JulianCode <hijuliansosa@protonmail.com>
2020-05-30 16:18:32 -0500

    Iteradores

---
+++ 07-iteradores.py
@@ -0,0 +1,15 @@
+lenguajes = ['Python', 'JavaScript', 'Dart', 'C++', 'Ruby']
+
+## Los iteradores corren determinado número de veces una pieza de código
+
+#iterador
+for lenguaje in lenguajes:
+    print(f'Estoy aprendiendo {lenguaje}')
+
+#for que escrina números
+for num in range(0, 20):
+    print(num)
+
+##Le podemos decir que incremente en cierta cantidad de números
+for num in range(0, 20, 3):
+    print(num)
