d20697cf1eae26a45415ae07a3ab8aabc0f9dc76
JulianCode <hijuliansosa@protonmail.com>
2020-05-30 00:45:53 -0500

    Variables

---
+++ 02-variables.py
@@ -0,0 +1,18 @@
+# hola_mundo = 'Hola mundo'
+
+# nombreCliente = 'Meraki' camelCase
+# NombreCliente = 'Meraki' Pascal Case o Capital CamelCase o TitleCase
+# nombrecliente = 'Meraki' flat case
+# nombre-cliente = 'Meraki' Kebab Case o Dash Case
+# nombre_cliente = 'Meraki' Snake Case recomendada
+
+# print(hola_mundo)
+
+# // Retos
+libro_favorito = 'Apocalipsis | Mario Mendoza'
+cancion_favorita = 'Stand By Me | Playing For Change'
+ultima_comida = 'Huevos y Chocolate'
+
+print(libro_favorito)
+print(cancion_favorita)
+print(ultima_comida)
