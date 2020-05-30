69c191ba20999b8d6fea46eacd1d0b2d980116d1
JulianCode <hijuliansosa@protonmail.com>
2020-05-30 00:53:31 -0500

    Tipos de datos

---
+++ 03-tipodedatos.py
@@ -0,0 +1,4 @@
+nombre = 'Meraki S' # Strings o Cadena de texto
+likes = 201 # Integers o Enteros
+total_pagar = 100.20# Floats o Flotantes
+pagado = True # Boolean, en Python los booleanos inician con Mayuscula True o False
