5312984c71deac46ffc5a16989041ac9b8ebc2f9
JulianCode <hijuliansosa@protonmail.com>
2020-05-30 01:06:54 -0500

    Funciones con Parametros y argumentos

---
+++ 05-funciones-2.py
@@ -0,0 +1,6 @@
+def informacion(nombre, puesto = 'Desconocido'):
+    print(f'Hola {nombre} y soy {puesto}')
+
+informacion('Meraki', 'Cantante')
+informacion('Ginna', 'Programadora')
+informacion('Julian')
