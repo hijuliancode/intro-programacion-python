e0b4ccd95659b19613984c8249aa9f546daf9155
JulianCode <hijuliansosa@protonmail.com>
2020-06-02 00:22:48 -0500

    Veamos que es el código condicional y unos ejemplos

---
+++ 08-condicionales.py
@@ -0,0 +1,23 @@
+# Revisar si una condición es mayor a
+balance = 0
+if balance > 0:
+  print('Puedes pagar')
+else:
+  print('No tienes saldo')
+
+# IF con texto
+lenguaje = 'Python'
+if lenguaje == 'Python':
+  print('Excelente decisión')
+
+## !== en Python es = a : not
+if not lenguaje == 'Java':
+  print('Excelente decisión')
+
+# Evaluar Boolean
+usuario_autenticado = True
+
+if usuario_autenticado:
+  print('El usuario esta autenticado')
+else:
+  print('El usuario no esta autenticado')
