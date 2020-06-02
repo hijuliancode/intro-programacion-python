28df297ab2c8c8bbc2461a593964cb41f9563d52
JulianCode <hijuliansosa@protonmail.com>
2020-06-02 10:07:21 -0500

    Lists, iteradores e If en un ejemplo

---
+++ 09-condicionales-y-iteradores.py
@@ -0,0 +1,7 @@
+lenguajes = ['Python', 'JavaScript', 'Dart', 'C++', 'Ruby']
+
+for lenguaje in lenguajes:
+    if lenguaje == 'Python':
+      print(f'Excelente {lenguaje} !!')
+    else:
+      print(lenguaje)
