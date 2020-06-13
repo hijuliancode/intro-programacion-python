659da4424a49be0060b6b2f549a1ac307051f9dd
JulianCode <hijuliansosa@protonmail.com>
2020-06-13 12:46:54 -0500

    Otros ejemplos con while

---
+++ 12-while.py
@@ -0,0 +1,14 @@
+numero = 0
+
+# while numero <= 10:
+#   print(numero)
+#   numero += 1 # Incremento para evitar un loop infinito
+
+# If dentro del while
+
+while numero <= 10:
+  if numero == 5:
+    print('Cincooo!')
+  else :
+    print(numero)
+  numero += 1
